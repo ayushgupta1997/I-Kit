a6ec3c808949b1f5234d88d7eddb1dac25fe8bf5
Ayush Gupta <39510321+ayushgupta1997@users.noreply.github.com>
2018-09-27 13:58:44 +0530

    Update Rotate Array.java

--- Rotate Array.java
+++ Rotate Array.java
@@ -1,3 +1,7 @@
+//method 1---juggling
+//Original List                   : 1 2 3 4 5 6 7
+  //   Result                     :  5 6 7 1 2 3 4
+
 public class Solution {
     public void rotate(int[] nums, int k) {
         k = k % nums.length;
@@ -13,6 +17,25 @@ public class Solution {
                 current = next;
                 count++;
             } while (start != current);
+        }
+    }
+}
+//method 2--(reverse)
+
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 }
